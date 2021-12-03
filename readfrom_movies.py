3b9285f1c3d766c3dd0b43ea48458c1cd7516f5c
丁尉洲 <joshua1300558237@126.com>
2021-12-03 16:34:33 -0500

    revise connection in readFrom_movies.py and have tested

--- readfrom_movies.py
+++ readfrom_movies.py
@@ -3,24 +3,17 @@
 
 # Note: the module name is psycopg, not psycopg3
 import psycopg2
-
+from connection import getConnection, queryUpdate, queryUpdate2
 # Connect to an existing database
-with psycopg2.connect("hostaddr=139.147.9.154 port=5432 dbname=core user=grp1admin") as conn:
-
-    # Open a cursor to perform database operations
-    with conn.cursor() as cur:
 
         # Query the database and obtain data as Python objects.
-        query = "SELECT id, name, year, rank "
-        query += "FROM  movies "
-        query += "ORDER BY year"
+query = "SELECT id, name, year, rank "
+query += "FROM  movies "
+query += "ORDER BY year"
 
-        cur.execute(query)
+results = queryUpdate2(query)
 
         # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
         # of several records, or even iterate on the cursor
-        for record in cur:
-            print(record)
-
-        # Make the changes to the database persistent
-        conn.commit()
+for record in results:
+    print(record)

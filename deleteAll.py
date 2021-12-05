ef3f2c08d7c8d02dcb504983f2650260790f48be
丁尉洲 <joshua1300558237@126.com>
2021-12-05 15:39:40 -0500

    revise deleteAll.py, add delete command for comments and review_comment

--- deleteAll.py
+++ deleteAll.py
@@ -3,10 +3,14 @@ conn = psycopg2.connect("hostaddr=139.147.9.154 port=5432 dbname=core user=grp1a
 with conn:
     with conn.cursor() as cur:
         query = "delete from review_user;"
+        query += "delete from review_comment;"
+        query += "delete from comments;"
         query += "delete from reviews;"
         query += "delete from votes;"
         query += "delete from users;"
+
         query += "alter sequence reviews_reviewid_seq restart;"
         query += "alter sequence votes_voteid_seq restart;"
         query += "alter sequence users_userid_seq  restart;"
+        query += "alter sequence comments_commentid_seq  restart;"
         cur.execute(query)

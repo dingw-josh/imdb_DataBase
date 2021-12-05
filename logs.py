c68a9aabde7bb4599127f28b903327f65371fda3
丁尉洲 <joshua1300558237@126.com>
2021-12-05 14:33:41 -0500

    refine logging format

--- logs.py
+++ logs.py
@@ -2,10 +2,11 @@ import logging
 
 import logging
 
-logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+#logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',datefmt='%d-%m-%Y:%H:%M:%S',level=logging.DEBUG,filename='loging.log')
+logging.basicConfig(format='%(asctime)s %(levelname)-8s  %(message)s',
     datefmt='%d-%m-%Y:%H:%M:%S',
     level=logging.DEBUG,
     filename='loging.log')
 
 def addLogs(input:str):
-    logging.info(str)
+    logging.info(input)

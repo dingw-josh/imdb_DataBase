15f8ad410dab57875a721865dfd457c82c07c920
丁尉洲 <joshua1300558237@126.com>
2021-12-05 17:15:15 -0500

    draft of main

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import createUser
+#import createUser
+import os
+# from multiprocessing import Pool
+# global count
+#
+# ids = [1,2,3,4,5,6,7,8]
+# def runUsers(id):
+#     os.system("python3 createUser.py " +str(id))
+#
+# def pool_func():
+#     p = Pool(8)
+#     p.map(runUsers,ids)
+#
+# if __name__ == '__main__':
+#     pool_func()
+from random import randrange
+import subprocess
 
+subprocess.call(['python3', 'createUser.py', str(1)])

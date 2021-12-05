3b7d0c7c12e2a93600fc1722f59108a6011ee5ef
丁尉洲 <joshua1300558237@126.com>
2021-12-05 16:12:37 -0500

    revise deleteALL

--- create_coreTables.py
+++ create_coreTables.py
@@ -1,5 +1,4 @@
 # Note: the module name is psycopg, not psycopg3
-import psycopg2
 from logs import addLogs
 from connection import queryUpdate, getConnection
 
--- deleteAll.py
+++ deleteAll.py
@@ -1,19 +1,19 @@
-import psycopg2
-conn = psycopg2.connect("hostaddr=139.147.9.154 port=5432 dbname=core user=grp1admin")
-with conn:
-    with conn.cursor() as cur:
-        query = "delete from review_user;"
-        query += "delete from review_comment;"
-        query += "delete from comments;"
-        query += "delete from reviews;"
-        query += "delete from votes;"
-        query += "delete from users;"
+from logs import addLogs
+from connection import queryUpdate, getConnection
 
-        query += "alter sequence reviews_reviewid_seq restart;"
-        query += "alter sequence votes_voteid_seq restart;"
-        query += "alter sequence users_userid_seq  restart;"
-        query += "alter sequence comments_commentid_seq  restart;"
-        cur.execute(query)
+
+query = "delete from review_user;"
+query += "delete from review_comment;"
+query += "delete from comments;"
+query += "delete from reviews;"
+query += "delete from votes;"
+query += "delete from users;"
+
+query += "alter sequence reviews_reviewid_seq restart;"
+query += "alter sequence votes_voteid_seq restart;"
+query += "alter sequence users_userid_seq  restart;"
+query += "alter sequence comments_commentid_seq  restart;"
+queryUpdate(query)
 
 file = open("loging.log","r+")
 file.truncate(0)

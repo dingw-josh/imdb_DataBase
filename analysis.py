b2f775b13d48e7c1263ccf2293bfd57bd268ce8a
丁尉洲 <joshua1300558237@126.com>
2021-12-06 02:34:35 -0500

    revise deleteALl.py, revise analysis.py, enable bestMovie()

--- analysis.py
+++ analysis.py
@@ -1,30 +1,35 @@
 from connection import getConnection, commit, queryUpdate, queryUpdate2
 
 def bestMovie():
+    #get all the movieids that are being surveied
     query = "select distinct movieid from surveys;"
     movieids = queryUpdate2(query)
     if not movieids:
         raise TypeError
-    print(len(results))
+    #print(len(movieids))
     totalScores = []
     for record in movieids:
-        query = "select * from surveys where moiveid = " + str(record[0]);
+        #get all the surveys that are about one movie
+        query = "select * from surveys where movieid = " + str(record[0]);
         surveyids = queryUpdate2(query)
+        #print( surveyids)
         if not surveyids:
             raise TypeError
+        movieScore = 0
         for record2 in surveyids:
-            query = "select * from survey_vote where movieid = " + str(record2[0]);
-            survey_votes = queryUpdate2(query)
-            movieScore = 0
+            #get all the votes that are related to one survey
+            surveyid = record2[0]
+            query = "select * from survey_vote where surveyid = " + str(surveyid);
+            survey_votes = queryUpdate2(query)[0]
             if not survey_votes:
                 raise TypeError
             for record3 in survey_votes:
-                query = "select * from votes where voteid = " + str(record3[1])
+                query = "select * from votes where voteid = " + str(record3)
                 votes = queryUpdate2(query)
                 if not votes:
                     raise TypeError
-                singleScore = int(votes[2]) + int(votes[3]) + int(votes[4])
-                query = "select usertype from users where userid = " + votes[1]
+                singleScore = int(votes[0][2]) + int(votes[0][3]) + int(votes[0][4])
+                query = "select usertype from users where userid = " + str(votes[0][1])
                 users = queryUpdate2(query)
                 if not users:
                     raise TypeError
@@ -33,6 +38,7 @@ def bestMovie():
                 elif users[0][0] == "User":
                     singleScore = singleScore * 3
                 movieScore = movieScore + singleScore
-            totalScores.append((movieScore,record2[0]))
+        totalScores.append((movieScore,record2[2]))
+    print(totalScores)
 
 bestMovie()
--- deleteAll.py
+++ deleteAll.py
@@ -4,6 +4,8 @@ from init_500Users import initUsers
 
 query = "delete from review_user;"
 query += "delete from review_comment;"
+query += "delete from survey_vote;"
+query += "delete from surveys;"
 query += "delete from comments;"
 query += "delete from reviews;"
 query += "delete from votes;"
@@ -12,6 +14,7 @@ query += "alter sequence reviews_reviewid_seq restart;"
 query += "alter sequence votes_voteid_seq restart;"
 query += "alter sequence users_userid_seq  restart;"
 query += "alter sequence comments_commentid_seq  restart;"
+query += "alter sequence surveys_surveyid_seq  restart;"
 queryUpdate(query)
 initUsers()
 
